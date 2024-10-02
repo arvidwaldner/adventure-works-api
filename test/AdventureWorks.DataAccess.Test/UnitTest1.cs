6a291fd39a684d5a911af98bd36d25156abde444
arvidwaldner <arvid.waldner87@gmail.com>
2024-10-02 11:56:43 +0200

    Adding more components.

---
+++ test/AdventureWorks.DataAccess.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AdventureWorks.DataAccess.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

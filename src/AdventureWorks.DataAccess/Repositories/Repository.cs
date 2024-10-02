e0beee6d54285bb700bbf7dd7d8f232a643942a0
arvidwaldner <arvid.waldner87@gmail.com>
2024-10-02 13:39:26 +0200

    Adding entity framework and entites genereated by database first approach.

---
+++ src/AdventureWorks.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,64 @@
+﻿using AdventureWorks.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks.DataAccess.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AdventureWorks2022Context _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AdventureWorks2022Context context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            T entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                _context.SaveChanges();
+            }
+        }
+    }
+}

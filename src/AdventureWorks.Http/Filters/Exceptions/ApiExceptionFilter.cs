6669d05fc963a550d6d65824a104964119bf6580
arvidwaldner <arvid.waldner87@gmail.com>
2024-10-03 14:44:18 +0200

    Adding exception and refactoring ExceptionFilter.

--- src/AdventureWorks.Http/Filters/Exceptions/ApiExceptionFilter.cs
+++ src/AdventureWorks.Http/Filters/Exceptions/ApiExceptionFilter.cs
@@ -12,33 +12,34 @@ namespace AdventureWorks.Http.Filters.Exceptions
 
             if(context.Exception is NotFoundException)
             {
-                problemDetails = new ProblemDetails
-                {
-                    Status = StatusCodes.Status404NotFound,
-                    Title = "Product not found",
-                    Detail = context.Exception.Message,
-                    Instance = context.HttpContext.Request.Path
-                };
-
+                problemDetails = MapProblemDetails(StatusCodes.Status404NotFound, "Product not found", context);
                 context.Result = new NotFoundObjectResult(problemDetails);
+            }
+            else if(context.Exception is BadRequestException)
+            {
+                problemDetails = MapProblemDetails(StatusCodes.Status400BadRequest, "Bad request", context);
+                context.Result = new BadRequestObjectResult(problemDetails);
             }
             else 
             {
-                problemDetails = new ProblemDetails
-                {
-                    Status = StatusCodes.Status500InternalServerError,
-                    Title = "An unexpected internal server error occured",
-                    Detail = context.Exception.Message,
-                    Instance = context.HttpContext.Request.Path
-                };
-
-                context.Result = new ObjectResult(problemDetails)
-                {
-                    StatusCode = StatusCodes.Status500InternalServerError
-                };
+                problemDetails = MapProblemDetails(StatusCodes.Status500InternalServerError, "Internal server error", context);
+                context.Result = new ObjectResult(problemDetails) { StatusCode = StatusCodes.Status500InternalServerError };
             }
 
             context.ExceptionHandled = true;
+        }
+
+        private ProblemDetails MapProblemDetails(int? status, string title, ExceptionContext exceptionContext)
+        {
+            var problemDetails = new ProblemDetails
+            {
+                Detail = exceptionContext.Exception.Message,
+                Status = status,
+                Instance = exceptionContext.HttpContext.Request.Path,
+                Title = title
+            };
+
+            return problemDetails;
         }
     }
 }
